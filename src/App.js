0033329934c639802c4732af9d47ab94aeb8930c
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-07-23 23:50:59 +0300

    feat:understand the useState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [restaurantName, setRestaurantName] =  useState("Lemon")
+  const [greetings, setGreetings] = useState("Hello, World!");
+  const [menu, setMenu] = useState("Loading...")
+  // console.log(restaurantName)
+
+function updateRestaurantName () {
+  setRestaurantName("Little lemon")
+}
+function updateGreetings () {
+  setGreetings("Welcome to Little Lemon! Our most modern restaurant!")
+}
+
+function updateMenu () {
+  setMenu("Hi! Chill and enjoy coffee as we work on it!")
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <h1>{restaurantName}</h1>
+          <button onClick={updateRestaurantName}>Updated Restaurant Name</button>
+          <h1>{greetings}</h1>
+          <button onClick={updateGreetings}>Learn more</button>
+          <h1>{menu}</h1>
+          <button onClick={updateMenu}>Checkout our Menu!!!</button>
     </div>
   );
 }
