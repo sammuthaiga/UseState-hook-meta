08e2aa3231c097de7120f4503e3f65f1d508e526
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-07-25 23:02:15 +0300

    Practice fetching data using the useEffect hook

---
+++ src/components/UseEfeect.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function App () {
+  const [btcData, setBtcData] = useState({});
+  const fetchData = () => {
+    fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`) 
+    .then((res) => res.json())
+    .then((jsonData) => setBtcData(jsonData.bpi.USD))
+    .catch((error) => console.log(error)); 
+  }
+  useEffect(() => {
+    fetchData()
+   }, [])
+  
+
+  return ( 
+    <> 
+      <h1>Current BTC/USD data</h1> 
+      <p>Code: {btcData.code}</p> 
+      <p>Symbol: {btcData.symbol}</p> 
+      <p>Rate: {btcData.rate}</p> 
+      <p>Description: {btcData.description}</p> 
+      <p>Rate Float: {btcData.rate_float}</p> 
+    </> 
+  ); 
+}
+
+export default App
